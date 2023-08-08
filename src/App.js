5f75f834d4ba238f23951fd2e6dbb32bc4656a07
Nicholas <jual31032@gmail.com>
2023-08-08 10:16:03 +0700

    latihann

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello, World
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code></code>Hello word.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.w3schools.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello word
+        </a>
+        <p>Hello word</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
